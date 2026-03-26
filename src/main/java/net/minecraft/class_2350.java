b579eb08c8f0065ee40945364921701a51a405df
PTOM76 <ptms76@outlook.jp>
2026-03-26 18:54:04 +0900

    feat: refactor class_2350 to use Holder for Direction constants and add values method

--- src/main/java/net/minecraft/class_2350.java
+++ src/main/java/net/minecraft/class_2350.java
@@ -5,12 +5,28 @@ import net.minecraft.core.Direction;
 // Direction
 public interface class_2350 {
 
-    Direction field_11043 = Direction.NORTH;
-    Direction field_11035 = Direction.SOUTH;
-    Direction field_11034 = Direction.EAST;
-    Direction field_11039 = Direction.WEST;
-    Direction field_11036 = Direction.UP;
-    Direction field_11033 = Direction.DOWN;
+//    class_2350 field_11043 = (class_2350) (Object) Direction.NORTH;
+//    class_2350 field_11035 = (class_2350) (Object) Direction.SOUTH;
+//    class_2350 field_11034 = (class_2350) (Object) Direction.EAST;
+//    class_2350 field_11039 = (class_2350) (Object) Direction.WEST;
+//    class_2350 field_11036 = (class_2350) (Object) Direction.UP;
+//    class_2350 field_11033 = (class_2350) (Object) Direction.DOWN;
+
+    class Holder {
+        static class_2350 north() { return (class_2350) (Object) Direction.NORTH; }
+        static class_2350 south() { return (class_2350) (Object) Direction.SOUTH; }
+        static class_2350 east() { return (class_2350) (Object) Direction.EAST; }
+        static class_2350 west() { return (class_2350) (Object) Direction.WEST; }
+        static class_2350 up() { return (class_2350) (Object) Direction.UP; }
+        static class_2350 down() { return (class_2350) (Object) Direction.DOWN; }
+    }
+
+    class_2350 field_11043 = Holder.north();
+    class_2350 field_11035 = Holder.south();
+    class_2350 field_11034 = Holder.east();
+    class_2350 field_11039 = Holder.west();
+    class_2350 field_11036 = Holder.up();
+    class_2350 field_11033 = Holder.down();
 
     default Direction asDirection() {
         return (Direction) (Object) this;
@@ -18,5 +34,9 @@ public interface class_2350 {
 
     default Direction method_10153() {
         return asDirection().getOpposite();
+    }
+
+    static class_2350[] values() {
+        return (class_2350[]) (Object[]) Direction.values();
     }
 }
