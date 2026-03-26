6f01e86367d49109c7c06669063a75a62d347f87
PTOM76 <ptms76@outlook.jp>
2026-03-26 18:15:24 +0900

    feat: add client and server runs configuration to build.gradle and create BlockItem subclass

---
+++ src/main/java/net/minecraft/class_1747.java
@@ -0,0 +1,10 @@
+package net.minecraft;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+
+public class class_1747 extends BlockItem {
+    public class_1747(Block block, Properties properties) {
+        super(block, properties);
+    }
+}
